05bd78ef9b7055923be9ade85b0a3f9cfbe13aae
Nipun Khanna <2Nipun@gmail.com>
2021-03-28 17:13:16 -0700

    wip - not tested

---
+++ amLibrary_ETLFunctions.py
@@ -0,0 +1,166 @@
+##############
+#### Common Library of ETL Functions ####
+#################
+## Feed it disease data, news data. Feed it queries. It parses and gives back just format needed ##
+##############
+## Ticket: https://www.notion.so/automedia/Create-data-service-for-pulling-from-disease-sh-7aded63dac7a47c9b6ca768356e4cb6d 
+#############
+
+
+## Declarations 
+import pandas as pd
+import os
+import json
+import ast #to covert string to list 
+
+### DATA
+### EXTRACTION using Pandas - Add more to get more out of data
+#Return data by region 
+def getSingleByRegion(allData, regionToGet, areaTable):
+	for i in allData:
+		if (i['region'] == regionToGet) and (i['areaTable'] == areaTable):
+			return i 
+	return [] #adding to return blank in case region not found, as error check
+
+# Get Top x by Use case
+def topListByTitle(allData, sortBy, filterBy, listHowMany):
+	df = pd.DataFrame(allData)
+	df_filtered = df.loc[df['areaTable'].isin([filterBy])] #Filters by US state or Country data 
+	sorted_df = df_filtered.sort_values(by = sortBy , ascending = False)
+	if 'World' in sorted_df.values: #To skip first value if World is in list, so hard coding for country vs states list 
+		rslt_df = sorted_df.head(listHowMany+1)[1:]
+	else:
+		rslt_df = sorted_df.head(listHowMany)
+	top_list_values = rslt_df.values.tolist()
+	top_list_headers = rslt_df.columns.tolist()
+
+	# There def must be a better pandas way of doing this, but for now couldnt figure out a way to export in format I wanted
+	final_list = []
+	for i in top_list_values:
+		temp_dict = {}
+		ind_temp = 0
+		for j in i:
+			temp_dict[top_list_headers[ind_temp]] = j
+			ind_temp += 1
+		final_list.append(temp_dict)
+	return final_list
+
+
+### TRANSFORMATION FUNCTIONS
+# Matching payload to get only limited items in mapping as wanted
+def dataSingleParse(payloadToCheck, dataToFillFrom):
+	outputDict = {}
+	# In payload the values are keys to data dump dict
+	for key, value in payloadToCheck['data_needed'].items():
+		if value in dataToFillFrom: 
+			if isinstance(dataToFillFrom[value], int):
+				outputDict[key] = ("{:,}".format(dataToFillFrom[value])) 
+			else:
+				outputDict[key] = dataToFillFrom[value] 
+	#Adding remaining values manually
+	outputDict['type'] = payloadToCheck['type']
+	outputDict['title'] = payloadToCheck['title']
+	outputDict['region'] = payloadToCheck['region']
+	return outputDict
+
+# Matching payload from dataTable
+def dataTableParse(payloadToCheck, dataListToFillFrom):
+	outputDict = {
+		"table_info": {
+			"type":payloadToCheck['type'],
+			"title":payloadToCheck['title'],
+			"sourceName":"",
+		},
+		"rows" : []
+	}
+	dataList = []
+	keyTemp = 0
+	for dataToFillFrom in dataListToFillFrom: 
+		tempDict = {}		
+		for key, value in payloadToCheck['data_needed'].items():
+			if value in dataToFillFrom: 
+				if isinstance(dataToFillFrom[value], int):
+					tempDict[key] = ("{:,}".format(dataToFillFrom[value])) 
+				else:
+					tempDict[key] = dataToFillFrom[value]
+		tempDict['key'] = str(keyTemp) 
+		keyTemp = keyTemp + 1 
+		dataList.append(tempDict)
+	outputDict["rows"] = dataList
+	outputDict["table_info"]["sourceName"] = dataList[0]["sourceName"]
+	return outputDict
+
+
+#### NEWS 
+# Gives back correct news data based on ask ie how many, and what format
+def getNewsData(inputNews, inputDataFormat):
+	type_asked = inputDataFormat["type"] #dataTable or dataSingle
+	data_asked = inputDataFormat["data_needed"] #format of data asked
+	
+	#Different functions if List or Single data asked for
+	if type_asked == 'dataSingle': #In case only 1 item asked for or available 
+		tempDict = {} #since this will return a single dict
+		recID_asked = inputDataFormat["recID_needed"] #which rec needed, only used if dataSingle
+		if recID_asked > len(inputNews):
+			return {"error":"🚫 Record asked not in dict"}
+		else:
+			for i in inputNews:
+				if (str(index(i)) == str(recID_asked)): #Only returning that recID
+				# if (str(i['recID']) == str(recID_asked)): #Only returning that recID
+					for key, value in data_asked.items(): #To map it
+						if value in i.keys(): 
+							tempDict[key] = i[value]
+			return tempDict
+	
+	elif type_asked == 'dataTable': #In case of newsTable
+		count_asked = inputDataFormat["count_needed"] #how many needed, only used if dataTable
+		range_to_check = count_asked if (count_asked <= len(inputNews)) else len(inputNews)
+		outputDict = {
+				"table_info": {
+					"type":"dataTable",
+				},
+				"rows" : []
+			}
+		outputList = []		
+		for rec in range(range_to_check): 
+			tempDict = {}
+			tempDict['recID'] = rec #to give it a sequence
+			dataIn = inputNews[rec] #already a dict
+			
+			for key, value in data_asked.items():
+				if value in dataIn: 
+					tempDict[key] = dataIn[value] 
+			outputList.append(tempDict)
+		outputDict["rows"] = outputList
+		return outputDict
+	
+	else:
+		return {"error":"🚫Data input incorrect"}
+
+
+#### COVID DATA 
+# Gives back correct news data based on ask ie how many, and what format
+def getCovidData(inputMasterDict, payload_json):
+
+	if "Prod_Ready" in i["fields"]: #Only working on prod ready ie checkboxed
+			type_asked = payload_json["type"] #Single data, or table of data 
+			series_workedon = i["fields"]["Series"] #to use for debug to check where failing
+
+			#Different functions if List or Single data asked for
+			if type_asked == "dataSingle":
+				region_asked = payload_json["region"] #USA, World etc
+				title_asked = payload_json["title"] #Cases, deaths etc
+				areaTable = payload_json["areaTable"] #All Countries data, or US State data
+				data_asked = getSingleByRegion(inputMasterDict, region_asked, areaTable) #Entire dict is sent
+				return data_asked
+
+			elif type_asked == "dataTable":
+				filterBy = payload_json["areaTable"] #So only that data goes
+				sortBy = payload_json["sortBy"] #Top by Cases, Deaths etc
+				listHowMany = int(payload_json["listHowMany"]) #Give back how many records
+				data_asked = topListByTitle(inputMasterDict, sortBy, filterBy, listHowMany)
+				return data_asked
+
+			else:
+				fields = {'data_output': "ERROR - Type incorrect"}
+				return data_asked
---
+++ task.py
@@ -0,0 +1,70 @@
+
+##############
+#### Airtable Data Service for CMS - ALl Services ####
+#################
+## Feed it news, covid data. Pulls payload from airtable service output per correct service & pulls output and uploads data in correct format as needed.  ##
+##############
+## Ticket:  
+#############
+
+## Declarations 
+import os
+from airtable import Airtable
+import json
+import ast #to covert string to list 
+from amLibrary_ETLFunctions import getCovidData, getNewsData
+
+# Airtable settings 
+base_key = os.environ.get("PRIVATE_BASE_KEY")
+table_producer = os.environ.get("PRIVATE_TABLE_NAME_PRODUCER")
+api_key_airtable = os.environ.get("PRIVATE_API_KEY_AIRTABLE")
+airtable_producer = Airtable(base_key, table_producer, api_key_airtable)
+
+### DATA UPLOAD FUNCTIONS
+#Uploads single json, or list to data_output of record ID as given
+def uploadData(inputDictList, recToUpdate):
+	recID = recToUpdate
+	if isinstance(inputDictList, dict):
+		fields = {'data_output': json.dumps(inputDictList)}
+		# fields = {'data_output': str(inputDictList)} #Seems if I do str thats same too
+	else:
+		fields = {'data_output': str(inputDictList)}
+	airtable_producer.update(recID, fields)
+
+
+
+#Goes through all records and updates ones that are in the master dict
+def updateLoop():
+	allRecords_covid = airtable.get_all(view='Service - amData')
+	allRecords_news = airtable_producer.get_all(view='Service - amDataNews')
+	allRecords_images = airtable_producer.get_all(view='Service - amImagePuller')
+	allRecords = allRecords_news + allRecords_images + allRecords_covid
+	# allRecords = airtable_producer.get_all()
+	for i in allRecords:
+		try: #In case have a prod payload or anything wrong 
+			if "Prod_Ready" in i["fields"]: #Only working on prod ready ie checkboxed
+				rec_ofAsked = i["id"]
+				payload_service = i["fields"]["am_Service"][0] #What service is data of
+				payload_native = i["fields"]["payload"] #Payload of data asked
+				payload_json = json.loads(payload_native)
+				type_asked = payload_json["type"] #Single data, or table of data 
+				service_output = i["fields"]["service_output"][0] #Main dict to be shared with all
+
+				if payload_service == "am_newspuller":
+					news_output = service_output #Since output is news 
+					news_output_json = ast.literal_eval(news_output) #since List from airtable is in String
+					data_toUpload = getNewsData(news_output_json, payload_json)
+					uploadData(data_toUpload, rec_ofAsked) #Just that bit updated 
+					print ("Row upload to CMS done..")
+
+				elif payload_service == "am_CovidData":
+					data_output = service_output #Since output is news 
+					data_toUpload = getCovidData(data_output, payload_json)
+					uploadData(data_toUpload, rec_ofAsked) #Just that bit updated 
+					print ("Row upload to CMS done..") 
+
+		except Exception: 
+			pass
+	print ("Upload to CMS done")
+
+updateLoop()
